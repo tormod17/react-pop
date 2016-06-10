d3c54dfd64c57b22503c7260613e784908b53f86
tormod17 <tormodsmith@gmail.com>
2016-06-10 14:20:22 +0100

    adding node modules to git ignore

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        preloaders: [{
+            test: /\.json$/,
+            loader: 'json'
+        }],
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
+        }, 
+        { 
+          test: /\.json$/, 
+          loader: "json-loader" 
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.json']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
+};
